08233b94800c0640e483980f87c9e5fcb5acab80
Emil Shpeklord <emil42661@gmail.com>
2022-04-28 09:10:30 +0300

    Initial Commit

---
+++ Registerr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Registerr
+//
+//  Created by Emil Shpeklord on 28.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
